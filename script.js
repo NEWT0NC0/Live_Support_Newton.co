cecd74ae741883e4865590a00e56d22e353eb8eb
NEWT0NC0 <www.newton.co@proton.me>
2025-04-24 22:37:24 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.getElementById('get-started').addEventListener('click', function() {
-    alert('Get started with Newton!');
-});
 
-document.getElementById('open-app').addEventListener('click', function() {
-    alert('Opening the app...');
-});
